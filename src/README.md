745fccf433ce1eff2f23aa53d1ae4ece9a0f7d31
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-09-17 11:30:32 +0200

    Finish the presentation of the syllaus

---
+++ src/README.md
@@ -0,0 +1,7 @@
+# Beamer template for LE2I - UB
+
+This repository hosts a template for presentation in beamer format. It is with the color of the LE2I and the Universite de Bourgogne
+
+Share and modify as you want.
+
+Guillaume Lemaitre
---
+++ src/auto/presentation.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook "presentation"
+ (lambda ()
+    (TeX-add-symbols
+     "tick"
+     "cross")
+    (TeX-run-style-hooks
+     "pifont"
+     "adjustbox"
+     "tikz-qtree"
+     "xifthen"
+     "tikz"
+     "transparent"
+     "subfigure"
+     "epstopdf"
+     "graphicx"
+     "epsf"
+     "amsmath"
+     "amsthm"
+     "amssymb"
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
